9254c19ee5f7bb06b5dcccfd8a7feafccada4663
Filipe Scaglia <filipecscaglia@gmail.com>
2023-09-02 19:54:53 -0300

    Feat: user check-ins history

---
+++ src/services/fetch-user-check-ins-history.ts
@@ -0,0 +1,23 @@
+import { CheckIn } from '@prisma/client';
+import { CheckInsRepository } from '@/repositories/check-ins-repository';
+
+interface FetchUserCheckInsServiceRequest {
+  userId: string;
+  page: number;
+}
+
+interface FetchUserCheckInsServiceResponse {
+  checkIns: CheckIn[];
+}
+
+export class FetchUserCheckInsService {
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+  ) {}
+
+  async execute({ userId, page }: FetchUserCheckInsServiceRequest): Promise<FetchUserCheckInsServiceResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(userId, page);
+
+    return { checkIns };
+  }
+}
