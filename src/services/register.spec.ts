14d53617efa49fd4501b74766f5470de2058f609
Filipe Scaglia <filipecscaglia@gmail.com>
2023-09-02 12:06:46 -0300

    Feat: test coverage

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -32,7 +32,7 @@ describe('Register Service', () => {
       password: '123456'
     });
 
-    expect(() => registerService.execute({
+    await expect(() => registerService.execute({
       name: 'Fulano',
       email,
       password: '123456'
