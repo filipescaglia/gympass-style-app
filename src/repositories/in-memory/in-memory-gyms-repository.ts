5bc17df974eb80e72be1f735a86a9ee5640570f3
Filipe Scaglia <filipecscaglia@gmail.com>
2023-09-02 16:51:51 -0300

    Feat: check-in date validation

---
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -0,0 +1,15 @@
+import { Gym } from '@prisma/client';
+import { GymsRepository } from '../gyms-repository';
+
+export class InMemoryGymsRepository implements GymsRepository {
+  public items: Gym[] = [];
+
+  async findById(id: string) {
+    const user = this.items.find(item => item.id === id);
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+}
