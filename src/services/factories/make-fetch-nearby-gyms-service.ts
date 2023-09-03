bc3fddd9ab02787f8c61bbae04bf0e8fdb751b2a
Filipe Scaglia <filipecscaglia@gmail.com>
2023-09-03 11:57:21 -0300

    Feat: factories

---
+++ src/services/factories/make-fetch-nearby-gyms-service.ts
@@ -0,0 +1,8 @@
+import { FetchNearbyGymsService } from '../fetch-nearby-gyms';
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository';
+
+export function makeFetchNearbyGymsService() {
+  const gymsRepository = new PrismaGymsRepository();
+  const service = new FetchNearbyGymsService(gymsRepository);
+  return service;
+}
---
+++ src/services/factories/make-fetch-user-check-ins-history-service.ts
@@ -0,0 +1,8 @@
+import { FetchUserCheckInsService } from '../fetch-user-check-ins-history';
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository';
+
+export function makeFetchUserCheckInsHistoryService() {
+  const checkInsRepository = new PrismaCheckInsRepository();
+  const service = new FetchUserCheckInsService(checkInsRepository);
+  return service;
+}
