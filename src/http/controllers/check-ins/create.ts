91a42d205926fc39e489674889be5ea89049b3c9
Filipe Scaglia <filipecscaglia@gmail.com>
2023-09-03 23:26:23 -0300

    Feat: check-in controllers

---
+++ src/http/controllers/check-ins/create.ts
@@ -0,0 +1,28 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { makeCheckInService } from '@/services/factories/make-check-in-service';
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createCheckInParamsSchema = z.object({
+    gymId: z.string().uuid()
+  });
+
+  const createCheckInBodySchema = z.object({
+    latitude: z.number().refine(value => Math.abs(value) <= 90),
+    longitude: z.number().refine(value => Math.abs(value) <= 180),
+  });
+
+  const { gymId } = createCheckInParamsSchema.parse(request.params);
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body);
+
+  const createCheckInService = makeCheckInService();
+
+  await createCheckInService.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+    gymId,
+    userId: request.user.sub,
+  });
+
+  return reply.status(201).send();
+}
