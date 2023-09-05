907b5e8cc56992adfb872fe1b7cfc4b84bd4d077
Filipe Scaglia <filipecscaglia@gmail.com>
2023-09-04 22:26:18 -0300

    Feat: role based authorization

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -16,18 +16,26 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 
     const { user } = await authenticateService.execute({ email, password });
 
-    const token = await reply.jwtSign({}, {
-      sign: {
-        sub: user.id,
-      }
-    });
+    const token = await reply.jwtSign(
+      {
+        role: user.role,
+      },
+      {
+        sign: {
+          sub: user.id,
+        }
+      });
 
-    const refreshToken = await reply.jwtSign({}, {
-      sign: {
-        sub: user.id,
-        expiresIn: '7d',
-      }
-    });
+    const refreshToken = await reply.jwtSign(
+      {
+        role: user.role,
+      },
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        }
+      });
 
     return reply
       .setCookie('refreshToken', refreshToken, {
